ee29df965984a885da3c4d4d130f04e55387ac86
Glitch (hello-webpage) <none>
2020-07-05 10:45:38 +0000

    🚘🎪 Checkpoint ./script.js:15784831/1905

--- script.js
+++ script.js
@@ -7,26 +7,30 @@ var ctx = c.getContext("2d");
 
 class Flower{
   constructor(){
-    this.xc = c.width * Math.random();
-    this.yd = c.height * Math.random();
+    this.xc = c.width * Math.random() | 0;
+    this.yd = -c.height * Math.random() | 0;
+    this.color = Math.random() * 360 | 0;
+    this.border = Math.random() * 360 | 0;
   }
   
   showFlower(){
     ctx.beginPath();
     ctx.moveTo(this.xc, this.yd);
-    for(let a = 0; a < 2*Math.PI*10;a+= 0.02){
+    for(let a = 0; a < 2*Math.PI;a+= 0.02){
       this.r = 20 * Math.cos(7 * a);
       this.x = this.xc + this.r * Math.cos(a);
       this.y = this.yd + this.r * Math.sin(a);
       ctx.lineWidth=1;
       ctx.lineTo(this.x,this.y);
     }
+    ctx.fillStyle="hsl("+this.color+",100%,50%)";
+    ctx.strokeStyle="hsl("+this.border+",100%,50%)";
     ctx.stroke();
     ctx.fill();
   }
   
   fallFlower(){
-    this.yd += 100;
+    this.yd += 5;
     if ( this.yd > c.height || this.yd < -c.height) {
       this.xc = Math.floor(c.width*Math.random() | 0);
       this.yd = Math.floor(-100 | 0);
@@ -35,56 +39,27 @@ class Flower{
   
 }
 
-
-
-
-
-
-// var xc = 400;
-// var yd = 400;
-// setInterval(()=>{
-//   ctx.clearRect(0,0,c.width, c.height);
-//   ctx.beginPath();
-//   ctx.moveTo(xc, yd);
-//   // var no = Math.floor(2+16*Math.random() | 0);
-//   // var xc = Math.floor(c.width*Math.random() | 0);
-//   // var yd = Math.floor(c.height*Math.random() | 0);
-//   ctx.fillStyle="hsl("+360+",100%,50%)";
-//   ctx.strokeStyle="hsl("+360+",100%,50%)";
-//   for(let a = 0; a < 2*Math.PI*10;a+= 0.02){
-//     var r = 50* Math.cos(7 * a);
-//     var x = xc + r * Math.cos(a);
-//     var y = yd + r * Math.sin(a);
-//     ctx.lineWidth=1;
-//     ctx.lineTo(x,y);
-//   }
-//   ctx.stroke();
-//   ctx.fill();
-//   yd += 10;
-//   if ( yd > c.height || yd < -c.height) {
-//     xc = Math.floor(c.width*Math.random() | 0);
-//     yd = Math.floor(-100 | 0);
-//   }
-// },50);
-
 let flowersArr = [];
 
 //create 1500 stars (objects)
-for (let i = 0; i < 1500; i++) flowersArr.push(new Flower());
-
-ctx.fillStyle="hsl(360,100%,50%)";
-ctx.strokeStyle="hsl(360,100%,50%)";
+for (let i = 0; i < 252; i++) flowersArr.push(new Flower());
 
 function draw() {
   ctx.clearRect(0,0,c.width, c.height);
   for (let s of flowersArr) {
-    s.fallFlower();
-    s.showFlower();
+    // setTimeout(()=>{
+      // draw();
+      s.fallFlower();
+      s.showFlower();
+    // },500);
   }
   //infinte call to draw
-  setInterval(()=>{
-    draw();
-  },500);
+  
+//      setTimeout(()=>{
+//       draw();
+//       // s.fallFlower();
+//       // s.showFlower();
+//     },10);
   // requestAnimationFrame(draw);
 }
 
