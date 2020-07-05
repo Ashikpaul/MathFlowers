d1672a8c4732890c5a43afe5dcf5fce03146ba4a
Glitch (hello-webpage) <none>
2020-07-05 09:24:14 +0000

    🤘🚗 Checkpoint ./script.js:15784831/1067

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 var c = document.getElementById("myCanvas");
-c.height = window.innerHeight;
-c.width = window.innerWidth;
+c.width=window.innerWidth;
+c.height=window.innerHeight;
 var ctx = c.getContext("2d");
 
-// setInterval(()=>{
 ctx.translate(c.width/2, c.height/2);
 
-var ca=Math.floor(c.width*Math.random());
-var d=Math.floor(c.height*Math.random());
-
-ctx.beginPath();
-ctx.moveTo(ca,d);
-ctx.fillStyle="hsl("+360*Math.random()+",100%,50%)";
-ctx.strokeStyle="hsl("+360*Math.random()+",100%,50%)";
-for(let a = 0; a < 2*Math.PI;a+= 0.0021){
-  var r = 350* Math.cos(7 * a);
-  var x = r * Math.sin(a);
-  var y = r * Math.cos(a);
-  ctx.lineWidth=3;
-  ctx.lineTo(x,y);
-  // ctx.stroke();
-  // ctx.fillStyle = "#ff2626"; // Red color
-  // ctx.fill(); 
-}
-ctx.stroke();
-ctx.fill();
-ctx.closePath()
-// },500);
+setInterval(()=>{
+  var no=Math.floor(2+16*Math.random());
+  var xc=Math.floor(c.width*Math.random() | 0);
+  var yd=Math.floor(c.height*Math.random() | 0);
+  ctx.beginPath();
+  ctx.moveTo(xc, yd);
+  ctx.fillStyle="hsl("+360*Math.random()+",100%,50%)";
+  ctx.strokeStyle="hsl("+360*Math.random()+",100%,50%)";
+  for(let a = 0; a < 2*Math.PI;a+= 0.01){
+    var r = ((xc+25)/5)* Math.cos(no * a);
+    var x = xc + r * Math.sin(a);
+    var y = yd + r * Math.cos(a);
+    ctx.lineWidth=3;
+    ctx.lineTo(x,y);
+  }
+  ctx.stroke();
+  ctx.fill();
+},1500);
 
 
