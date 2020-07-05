baf843f96503e7bc984549fe56933cd928869c7b
Glitch (hello-webpage) <none>
2020-07-05 09:36:14 +0000

    📊🏀 Checkpoint ./script.js:15784831/513

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ c.width=window.innerWidth;
 c.height=window.innerHeight;
 var ctx = c.getContext("2d");
 
-ctx.translate(c.width/2, c.height/2);
+// ctx.translate(c.width/2, c.height/2);
 
 setInterval(()=>{
   var no=Math.floor(2+16*Math.random());
@@ -13,15 +13,15 @@ setInterval(()=>{
   ctx.moveTo(xc, yd);
   ctx.fillStyle="hsl("+360*Math.random()+",100%,50%)";
   ctx.strokeStyle="hsl("+360*Math.random()+",100%,50%)";
-  for(let a = 0; a < 2*Math.PI;a+= 0.01){
-    var r = ((xc+25)/5)* Math.cos(no * a);
+  for(let a = 0; a < 2*Math.PI*10;a+= 0.02){
+    var r = 300* Math.cos((5/8) * a);
     var x = xc + r * Math.sin(a);
     var y = yd + r * Math.cos(a);
-    ctx.lineWidth=3;
+    ctx.lineWidth=1;
     ctx.lineTo(x,y);
   }
   ctx.stroke();
   ctx.fill();
-},1500);
+},5000);
 
 
