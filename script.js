7315f7fd1284eb7cdca20dc547beede2448392a3
Glitch (hello-webpage) <none>
2020-07-05 07:24:45 +0000

    🗽🌲 Checkpoint ./style.css:15784831/139 ./script.js:15784831/940 ./index.html:15784831/448

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var c = document.getElementById("myCanvas");
+c.height = window.innerHeight;
+c.width = window.innerWidth;
+var ctx = c.getContext("2d");
+ctx.translate(c.width/2, c.height/2);
+for(let a = 0; a < 2*Math.PI;a+= 0.02){
+  var r = 200* Math.cos(7 * a);
+  var x = r * Math.cos(a);
+  var y = r * Math.sin(a);
+  ctx.fillStyle = "#ff2626"; // Red color
+  ctx.beginPath(); //Start path
+  ctx.arc(x, y, 5, 0, Math.PI * 2, true); 
+  ctx.fill(); 
+  ctx.stroke();
+}
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
