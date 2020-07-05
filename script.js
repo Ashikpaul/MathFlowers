5146b1ac4394ac86f11858fef67546c2199d55f7
Glitch (hello-webpage) <none>
2020-07-05 09:12:12 +0000

    🎍🚂 Checkpoint ./script.js:15784831/1625

--- script.js
+++ script.js
@@ -4,30 +4,28 @@ c.width = window.innerWidth;
 var ctx = c.getContext("2d");
 
 // setInterval(()=>{
-  ctx.translate(c.width/2, c.height/2);
-  ctx.clearRect(0,0,c.width, c.height);
-  for(let a = 0; a < 2*Math.PI;a+= 0.0021){
-    var r = 350* Math.cos(7 * a);
-    var x = r * Math.sin(a);
-    var y = r * Math.cos(a);
-    ctx.beginPath(); //Start path
-    ctx.arc(x, y, 5, 0, Math.PI * 2, true); 
-    ctx.stroke();
-    ctx.fillStyle = "#ff2626"; // Red color
-    ctx.fill(); 
-  }
+ctx.translate(c.width/2, c.height/2);
 
-  for(let a = 0; a < 2*Math.PI;a+= 0.0021){
-    var r = 350* Math.sin(7 * a);
-    var x = r * Math.cos(a);
-    var y = r * Math.sin(a);
-    ctx.fillStyle = "#ffAA26"; // Red color
-    ctx.beginPath(); //Start path
-    ctx.arc(x, y, 5, 0, Math.PI * 2, true); 
-    ctx.fill(); 
-    ctx.closePath();
-    ctx.stroke();
-  }
+var ca=Math.floor(c.width*Math.random());
+var d=Math.floor(c.height*Math.random());
+
+ctx.beginPath();
+ctx.moveTo(ca,d);
+ctx.fillStyle="hsl("+360*Math.random()+",100%,50%)";
+ctx.strokeStyle="hsl("+360*Math.random()+",100%,50%)";
+for(let a = 0; a < 2*Math.PI;a+= 0.0021){
+  var r = 350* Math.cos(7 * a);
+  var x = r * Math.sin(a);
+  var y = r * Math.cos(a);
+  ctx.lineWidth=3;
+  ctx.lineTo(x,y);
+  // ctx.stroke();
+  // ctx.fillStyle = "#ff2626"; // Red color
+  // ctx.fill(); 
+}
+ctx.stroke();
+ctx.fill();
+ctx.closePath()
 // },500);
 
 
