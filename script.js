9b408b68e1ef8b5eb5be0bede6b3f2ca2c32b92d
Glitch (hello-webpage) <none>
2020-07-05 09:47:32 +0000

    🎨🌦 Checkpoint ./script.js:15784831/1768

--- script.js
+++ script.js
@@ -5,23 +5,27 @@ var ctx = c.getContext("2d");
 
 // ctx.translate(c.width/2, c.height/2);
 
+var xc = 400;
+var yd = 400;
 setInterval(()=>{
-  var no=Math.floor(2+16*Math.random());
-  var xc=Math.floor(c.width*Math.random() | 0);
-  var yd=Math.floor(c.height*Math.random() | 0);
+  ctx.clearRect(0,0,c.width, c.height);
   ctx.beginPath();
   ctx.moveTo(xc, yd);
+  // var no = Math.floor(2+16*Math.random() | 0);
+  // var xc = Math.floor(c.width*Math.random() | 0);
+  // var yd = Math.floor(c.height*Math.random() | 0);
   ctx.fillStyle="hsl("+360*Math.random()+",100%,50%)";
   ctx.strokeStyle="hsl("+360*Math.random()+",100%,50%)";
   for(let a = 0; a < 2*Math.PI*10;a+= 0.02){
-    var r = 300* Math.cos((5/8) * a);
-    var x = xc + r * Math.sin(a);
-    var y = yd + r * Math.cos(a);
+    var r = 200* Math.cos(7 * a);
+    var x = xc + r * Math.cos(a);
+    var y = yd + r * Math.sin(a);
     ctx.lineWidth=1;
     ctx.lineTo(x,y);
   }
   ctx.stroke();
   ctx.fill();
-},5000);
+  yd += 10;
+},100);
 
 
